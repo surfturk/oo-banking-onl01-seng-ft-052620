5f85f1cffc32c10d9ed0cde1b569a7ea46a4391f
Berkley Brown <berkleydeane@gmail.com>
2020-06-10 20:42:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,10 +18,7 @@ class Transfer
   
   
   def execute_transaction
-    
-
-    
-    if status = "pending" && self.valid? && sender.balance > 0
+      if status = "pending" && self.valid? && sender.balance > 0
       
     receiver.balance += amount
     sender.balance -= amount
