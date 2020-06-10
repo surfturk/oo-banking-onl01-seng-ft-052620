524de22ba4427b0ea506d06773f584919b673595
Berkley Brown <berkleydeane@gmail.com>
2020-06-10 20:40:46 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,35 @@
 class Transfer
-  # your code here
+  attr_accessor :sender, :receiver, :amount, :status
+  
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end  
+  
+  def valid?
+    if sender.valid? == true && receiver.valid? == true
+      true
+    else
+      false  
+    end
+  end  
+  
+  
+  def execute_transaction
+    
+
+    
+    if status = "pending" && self.valid? && sender.balance > 0
+      
+    receiver.balance += amount
+    sender.balance -= amount
+  else
+    false
+  end  
+  self.clear
+ end  
+  
+  
 end
